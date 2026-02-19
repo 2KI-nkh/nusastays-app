8c23f71d221f2c67c84799ee718237585af9ba51
2KI-nkh <129398495+2KI-nkh@users.noreply.github.com>
2026-02-18 16:17:59 -0800

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import VideoGenerationPanel from './components/video/VideoGenerationPanel';
+
+function App() {
+  return (
+    <div style={{ padding: '1rem' }}>
+      <h1>NusaStays Video Generator</h1>
+      <VideoGenerationPanel />
+    </div>
+  );
+}
+
+export default App;
